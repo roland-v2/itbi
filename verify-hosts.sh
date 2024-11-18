ceaf977d0b04abfda72801ff971b1d5b5a7e6956
roland-v2 <rboldesco14@gmail.com>
2024-11-18 17:43:36 +0200

    update script

--- verify-hosts.sh
+++ verify-hosts.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-grep -P '^d+(\.\d+){3}' /etc/hosts | while read line; do
-  ip=$(echo line | awk '{print $1}')
-  host=$(echo line | awk '{print $2}')
-  ns_ip=$(nslookup $host 1.1.1.1 | awk '/^Address: / { print $2 }' | head -n 1)
+
+validate_ip() {
+  local host=$1
+  local ip=$2
+  local dns_server=$3
+
+  ns_ip=$(nslookup $host $dns_server | awk '/^Address: / { print $2 }' | head -n 1)
   if [[ $ip != $ns_ip ]]; then
-    echo "bogus ip for $host in /etc/hosts ($ip, got $ns_ip)
+    echo "ip for $host ($ip) is wrong. $ns_ip is correct"
   fi
-done
+}
+
+validate_ip $1 $2 $3
